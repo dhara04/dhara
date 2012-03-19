af9493b3eefa1553b84357ea50fc1938293d1121
dhara doshi <dhara04@gmail.com>
2012-03-19 23:37:48 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dhara::Application.config.secret_token = 'c94451e2c8b98e3ff58c2aa5026709299f5f8fd3a64436b365bc6d1f2666919d675db087e6fbc1102d322d8653e4653934cc14bf04043dc8ea28891a98033bff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Dhara::Application.config.session_store :cookie_store, key: '_dhara_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Dhara::Application.config.session_store :active_record_store
